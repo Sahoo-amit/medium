754549ad71b962a4235dc15e3f8cf6d6c4954060
Amit Sahoo <amitsahooamit456@gmail.com>
2025-07-22 13:13:04 +0530

    minor bug fixed

--- client/src/components/Skeleton.jsx
+++ client/src/components/Skeleton.jsx
@@ -1,6 +1,4 @@
 const CardSkeleton = () => {
-  const theme = ThemeStore((state) => state.theme);
-  const isDark = theme === "dark";
 
   return (
     <div className="flex flex-row-reverse justify-between gap-10 items-start">
